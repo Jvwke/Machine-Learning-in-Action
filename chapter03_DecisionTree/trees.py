02741ef2273cbb53ec0fe17b274f0e67b86f57d3
orzyt <xmzyt1996@gmail.com>
2017-07-27 22:21:39 +0800

    update trees.py

--- chapter03_DecisionTree/trees.py
+++ chapter03_DecisionTree/trees.py
@@ -1,4 +1,5 @@
 from math import log
+import operator
 
 def calcShannonEnt(dataSet):
     n = len(dataSet)
@@ -53,6 +54,29 @@ def chooseBestFeatureToSplit(dataSet):
             bestFeature = i
     return bestFeature
 
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList): return classList[0]
+    if len(dataSet[0]) == 1: return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
 if __name__ == '__main__':
     myDat, labels = createDataSet()
     entropy = calcShannonEnt(myDat)
@@ -61,3 +85,5 @@ if __name__ == '__main__':
     print(splitDataSet(myDat, 0, 0))
     bestFeature = chooseBestFeatureToSplit(myDat)
     print(bestFeature)
+    myTree = createTree(myDat, labels)
+    print(myTree)
