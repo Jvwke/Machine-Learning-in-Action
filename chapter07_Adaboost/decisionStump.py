708b33e080dffab446dcf9a3a6aaf637c31911c6
orzyt <xmzyt1996@gmail.com>
2017-10-15 16:54:50 +0800

    add: a weak learner with decision stumps

---
+++ chapter07_Adaboost/decisionStump.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def loadSimpData():
+
+    datMat = np.matrix([[1., 2.1],
+                        [1.5, 1.6],
+                        [1.3, 1.],
+                        [1., 1.],
+                        [2., 1.]])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return datMat, classLabels
+
+
+def showDataSet(datMat, labelMat):
+    xcord_plus = []; ycord_plus = []
+    xcord_minus = []; ycord_minus = []
+    for i in range(len(datMat)):
+        if labelMat[i] == 1.0:
+            xcord_plus.append(datMat[i, 0]), ycord_plus.append(datMat[i, 1])
+        else:
+            xcord_minus.append(datMat[i, 0]), ycord_minus.append(datMat[i, 1])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord_plus, ycord_plus, marker='o')
+    ax.scatter(xcord_minus, ycord_minus, marker='s')
+    plt.show()
+
+
+def stumpClassify(dataMatrix, dimen, threshVal, threshIneq):
+    retArray = np.ones((np.shape(dataMatrix)[0], 1))
+    if threshIneq == 'lt':
+        retArray[dataMatrix[:, dimen] <= threshVal] = -1.0
+    else:
+        retArray[dataMatrix[:, dimen] > threshVal] = -1.0
+    return retArray
+
+
+def buildStump(dataArr, classLabels, D):
+    dataMatrix = np.mat(dataArr)
+    labelMat = np.mat(classLabels).T
+    m, n = np.shape(dataMatrix)
+    numSteps = 10.0
+    bestStump = {}
+    bestClasEst = np.mat(np.zeros((m, 1)))
+    minError = float('inf')
+    for i in range(n):
+        rangeMin = dataMatrix[:, i].min()
+        rangeMax = dataMatrix[:, i].max()
+        stepSize = (rangeMax - rangeMin) / numSteps
+        for j in range(-1, int(numSteps) + 1):
+            for inequal in ['lt', 'gt']:
+                threshVal = (rangeMin + float(j) * stepSize)
+                predictedVals = stumpClassify(dataMatrix, i, threshVal, inequal)
+                errArr = np.mat(np.ones((m, 1)))
+                errArr[predictedVals == labelMat] = 0
+                weightedError = D.T * errArr
+                # print("split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" % (i, threshVal, inequal, weightedError))
+                if weightedError < minError:
+                    minError = weightedError
+                    bestClasEst = predictedVals.copy()
+                    bestStump['dim'] = i
+                    bestStump['thresh'] = threshVal
+                    bestStump['ineq'] = inequal
+    return bestStump, minError, bestClasEst
+
+
+if __name__ == '__main__':
+    dataArr, classLabels = loadSimpData()
+    showDataSet(dataArr, classLabels)
+    dataArr,classLabels = loadSimpData()
+    D = np.mat(np.ones((5, 1)) / 5)
+    bestStump,minError,bestClasEst = buildStump(dataArr,classLabels,D)
+    print('bestStump:\n', bestStump)
+    print('minError:\n', minError)
+    print('bestClasEst:\n', bestClasEst)
